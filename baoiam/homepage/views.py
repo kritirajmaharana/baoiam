76df574d5e5527abadc3e19f19af96967652f94f
ayushi jain <ayushijain.jain12345@gmail.com>
2023-11-30 23:17:05 +0530

    urls.py file is fully changed -change in views.py file -change in register.html -new application app homepage are created

--- baoiam/homepage/views.py
+++ baoiam/homepage/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 # def home(request):
 #     return render(request, 'home/index.html')
 
-# def contactus(request):
-#     return render(request, 'contact-page/index.html')
+# Create your views here.
